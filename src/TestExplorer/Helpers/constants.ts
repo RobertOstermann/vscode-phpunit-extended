81d4209700262320e5bdd5e01734a9d0e9ce96a7
Robbie Ostermann <robertostermann5@gmail.com>
2021-11-04 20:11:20 -0500

    Improve REGEX constants

--- src/TestExplorer/Helpers/constants.ts
+++ src/TestExplorer/Helpers/constants.ts
@@ -7,12 +7,12 @@ export class Constants {
   static readonly individualTestPassedMessage: string = "OK";
   static readonly invalidTestMessage: string = "Test Failed: Check Terminal Output";
 
-  static readonly classOutputRegex = /(Tests:.*Assertions.*Failures(\w*[^\\S\r\n\.])*)/is;
+  static readonly classOutputRegex = /(Tests:.*Assertions.*(Incomplete|Risky|Skipped|Failures)(?:\w*[^\r\n\.])*)/is;
 
   static readonly phpUnitSuccessRegex = /OK\s+\(.*\)/i;
-  static readonly phpUnitFailureRegex = /Failed\s+(\w*\s*)/i;
-  static readonly phpUnitNoTestsRegex = /No\stests(\w*\s*)/i;
-  static readonly phpUnitNoAssertionsRegex = /.*not\sperform\sany\sassertions(\w*\s*)/i;
+  static readonly phpUnitFailureRegex = /Failed\s+(?:[^\.])*/i;
+  static readonly phpUnitNoTestsRegex = /No tests executed/i;
+  static readonly phpUnitNoAssertionsRegex = /This test did not perform any assertions/i;
 
   static readonly phpClassRegex = /^\s*class\s+(\w*)\s*{?/gi;
   static readonly phpMethodRegex = /\s*(public\s+)?function\s+(\w+)\s*\(/gi;
